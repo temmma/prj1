b5220b22691b8f3021e18179c5603ae4df8c76c3
Artem Ovodov <artovod@gmail.com>
2013-05-08 10:07:35 +0400

    d

--- prj1/src/prj1/test.java
+++ prj1/src/prj1/test.java
@@ -2,6 +2,8 @@ package prj1;
 public class test {
 	public static void main(String[] args) throws Exception {
 		System.out.println("It's not very funny!");
+		System.out.println("And something new.");
+		System.out.println("Something changed.");
 	}
 
 }
