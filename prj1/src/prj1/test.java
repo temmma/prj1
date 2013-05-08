d2ba59fa4dc2e732c7f0e4b107363c7cde5bb765
Artem Ovodov <artovod@gmail.com>
2013-05-08 09:59:56 +0400

    new commit

--- prj1/src/prj1/test.java
+++ prj1/src/prj1/test.java
@@ -1,7 +1,7 @@
 package prj1;
 public class test {
 	public static void main(String[] args) throws Exception {
-		System.out.println("Where is code??");
+		System.out.println("It's not very funny!");
 	}
 
 }
